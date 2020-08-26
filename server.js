e340f1975d7cd2ce1720c7d08917504716c27822
SH13LDS <coalshields@gmail.com>
2020-08-26 15:27:44 -0500

    Basic route setup, tested in Postman

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/mountains', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all mountains' });
+});
+
+app.get('/api/v1/mountains/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show mountain ${req.params.id}` });
+});
+
+app.post('/api/v1/mountains', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new mountain' });
+});
+
+app.put('/api/v1/mountains/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update mountain ${req.params.id}` });
+});
+
+app.delete('/api/v1/mountains/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete mountain ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
