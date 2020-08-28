70e6ba92c36c1d5417d5e01f2100e32d08d4037c
SH13LDS <coalshields@gmail.com>
2020-08-27 20:57:34 -0500

    Mongoose duplicate key error handling

--- middleware/error.js
+++ middleware/error.js
@@ -5,11 +5,18 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for the developer
-  console.log(err.stack);
+  console.log(err);
+
   // Mongoose bad ObjectId
   if (err.name === 'Cast Error') {
-    const message = `Mountain with id of ${err.value} not found`;
+    const message = `Mountain with id of ${err.value} not found.`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered.`;
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
